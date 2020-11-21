d13662c01a931ab28c987034c33283124c815557
Martin Sundhaug <martinsundhaug@gmail.com>
2020-11-21 14:25:40 +0100

    Prettifier avoid mojibake

--- tools/json-prettifier/prettifier.py
+++ tools/json-prettifier/prettifier.py
@@ -4,4 +4,4 @@ import glob
 for checklist in glob.glob('**/*.json', recursive=True) + glob.glob('**/*.desc', recursive=True):
     print(checklist)
     j = json.load(open(checklist))
-    open(checklist, 'w').write(json.dumps(j, indent=4, sort_keys=True))
+    open(checklist, 'w').write(json.dumps(j, ensure_ascii=False, indent=4, sort_keys=True))
